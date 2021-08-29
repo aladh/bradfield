b3d2894a88bb21047e1412a199b9bc9aef1f076a
Alishan Ladhani <git@alad.ca>
2021-08-29 14:54:56 -0400

    Extract fetcher package

---
+++ 0_prep/2_go/xkcd_search/fetcher/fetcher.go
@@ -0,0 +1,42 @@
+package fetcher
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+const baseURL = "https://xkcd.com"
+const filename = "info.0.json"
+const notFound = 404
+
+func Fetch(skip func(int) bool, store func(int, io.Reader) error) error {
+	for i := 2400; true; i++ {
+		log.Printf("fetching comic %d\n", i)
+
+		if skip(i) {
+			log.Printf("skip fetching comic %d because a file already exists\n", i)
+			continue
+		}
+
+		resp, err := http.Get(fmt.Sprintf("%s/%d/%s", baseURL, i, filename))
+		if err != nil {
+			return fmt.Errorf("error making request for comic %d: %w", i, err)
+		}
+
+		if resp.StatusCode == notFound {
+			log.Printf("comic %d not found - stopped fetching\n", i)
+			break
+		}
+
+		err = store(i, resp.Body)
+		if err != nil {
+			return err
+		}
+
+		_ = resp.Body.Close()
+	}
+
+	return nil
+}
--- 0_prep/2_go/xkcd_search/index/index.go
+++ 0_prep/2_go/xkcd_search/index/index.go
@@ -1,62 +1,50 @@
 package index
 
 import (
-	"errors"
 	"fmt"
 	"io"
-	"io/fs"
-	"log"
-	"net/http"
 	"os"
 	"path/filepath"
-)
 
-const baseURL = "https://xkcd.com"
-const externalFilename = "info.0.json"
-const notFound = 404
+	"github.com/aladh/bradfield/0_prep/2_go/xkcd_search/fetcher"
+)
 
 const storageDirectory = "comics"
 
 func Create() error {
-	for i := 2500; true; i++ {
-		log.Printf("fetching comic %d\n", i)
-
-		filename := fmt.Sprintf("%s/%d.json", storageDirectory, i)
-
-		path, err := filepath.Abs(filename)
-		if err != nil {
-			return fmt.Errorf("error building file path for comic %d: %w", i, err)
-		}
-
-		if _, err := os.Stat(path); err == nil {
-			log.Printf("skip fetching comic %d because a file already exists\n", i)
-			continue
-		} else if !errors.Is(err, fs.ErrNotExist) {
-			return fmt.Errorf("error checking existing file for comic %d: %w", i, err)
-		}
-
-		resp, err := http.Get(fmt.Sprintf("%s/%d/%s", baseURL, i, externalFilename))
-		if err != nil {
-			return fmt.Errorf("error making request for comic %d: %w", i, err)
-		}
-
-		if resp.StatusCode == notFound {
-			log.Printf("comic %d not found - stopping index creation\n", i)
-			break
-		}
-
-		f, err := os.Create(path)
-		if err != nil {
-			return fmt.Errorf("error creating file for comic %d: %w", i, err)
-		}
-
-		_, err = io.Copy(f, resp.Body)
-		if err != nil {
-			return fmt.Errorf("error writing file for comic %d: %w", i, err)
-		}
-
-		_ = resp.Body.Close()
+	err := fetcher.Fetch(skip, store)
+	if err != nil {
+		return err
 	}
 
 	return nil
+}
+
+func skip(comicNum int) bool {
+	if _, err := os.Stat(path(comicNum)); err == nil {
+		return true
+	}
+
+	return false
+}
+
+func store(comicNum int, comicReader io.Reader) error {
+	f, err := os.Create(path(comicNum))
+	if err != nil {
+		return fmt.Errorf("error creating file for comic %d: %w", comicNum, err)
+	}
+
+	_, err = io.Copy(f, comicReader)
+	if err != nil {
+		return fmt.Errorf("error writing file for comic %d: %w", comicNum, err)
+	}
+
+	return nil
+}
+
+func path(comicNum int) string {
+	filename := fmt.Sprintf("%s/%d.json", storageDirectory, comicNum)
+	path, _ := filepath.Abs(filename)
+
+	return path
 }
