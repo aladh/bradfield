dfa903234450de64f40f8c8aced1fbe1dce02b89
Alishan Ladhani <git@alad.ca>
2022-06-01 20:54:18 -0400

    Reject invalid commands

--- 7_distributed_systems/kv_store/main.go
+++ 7_distributed_systems/kv_store/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 )
 
+const GetCommand = "get"
+const SetCommand = "set"
+
 func main() {
 	var command string
 	var arg string
@@ -15,6 +18,11 @@ func main() {
 		_, err := fmt.Scanf("%s %s", &command, &arg)
 		if err != nil {
 			log.Fatalf("error reading input: %s\n", err)
+		}
+
+		if command != GetCommand && command != SetCommand {
+			fmt.Printf("invalid command: %s\n", command)
+			continue
 		}
 
 		fmt.Println(command, arg)
